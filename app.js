bd9bc8ebd0dd0b46ee8ee74f509cb0cf67ce6005
Mahak491 <shreemahak01@gmail.com>
2022-06-05 17:34:27 +0530

    banana talk

--- app.js
+++ app.js
@@ -2,11 +2,30 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
+
+
+function getTranslationUrl(text){
+return serverURL + "?" + "text=" + text
+}
+
+function errorHandler(error){
+    console.log("error occured",error);
+    alert("something wrong with server! try again after some time")
+}
 
 function clickHandler(){
-    outputDiv.innerText = "Mahak shree"
-} 
+    // outputDiv.innerText = "Mahak shree"
+    var inputText = txtInput.value;
+    fetch(getTranslationUrl(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;
+        })
+    .catch(errorHandler)
+};
 
 btnTranslate.addEventListener("click",clickHandler)
         
