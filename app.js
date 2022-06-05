46bde1ab2ffb180629ebb9bf364d01fe0aba1028
Mahak491 <shreemahak01@gmail.com>
2022-06-05 14:56:08 +0530

    input output processing

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-var username = prompt("Give me username");
-var welcomeMessage = "this script works"+username;
-alert(welcomeMessage);
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+
+
+function clickHandler(){
+    outputDiv.innerText = "Mahak shree"
+} 
+
+btnTranslate.addEventListener("click",clickHandler)
+        
+    
